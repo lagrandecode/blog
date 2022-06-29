876a753572e42d85645aebdafec338c0354a27de
lagrandecode <seunogunmolufirst1@gmail.com>
2022-06-29 18:56:02 +0100

    "commit"

--- blog/urls.py
+++ blog/urls.py
@@ -2,13 +2,13 @@
 from sqlite3 import connect
 from django.contrib import admin
 from django.urls import path, include
-from connect.views import question_list, question_retrieve
-from connect.views import question_create, question_update
+from connect.views import (question_list, question_retrieve, question_create, question_update, question_delete)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', question_list),
     path('retrieve/<pk>/', question_retrieve),
     path('create/',question_create),
-    path('retrieve/<pk>/edit/', question_update)
-] 
+    path('retrieve/<pk>/edit/', question_update),
+    path('retrieve/<pk>/delete/', question_delete)
+]     
