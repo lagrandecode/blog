884f3b3daeecbed98249fb6a6a5e6d9f5cfa3de5
lagrandecode <seunogunmolufirst1@gmail.com>
2022-06-29 13:24:46 +0100

    "commit"

--- connect/models.py
+++ connect/models.py
@@ -1,3 +1,15 @@
+from pyexpat import model
+from turtle import title
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=200)
+    grade = models.CharField(max_length=50)
+    cadre = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+
+    
