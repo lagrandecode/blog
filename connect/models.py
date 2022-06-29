370cf0f1c7980d60005081087664401cf58adfcb
lagrandecode <seunogunmolufirst1@gmail.com>
2022-06-29 14:26:22 +0100

    "commit"

--- connect/models.py
+++ connect/models.py
@@ -11,5 +11,8 @@ class Question(models.Model):
     address = models.CharField(max_length=50)
     age = models.IntegerField() 
 
+    def __str__(self):
+        return self.title
+
 
 
--- connect/views.py
+++ connect/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 
 #creating starting page 
 
+
 def index(request):
     return HttpResponse('hello world')
+
+#lets perform crud operation 
+
+def question_list(request):
+    question = Question.objects.all()
+    context = {
+        'question' : question
+    }
+    return render(request, 'questionlist.html', context)
